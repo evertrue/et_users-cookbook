9ee688aca0e4ed2e6f765f53b12aa7b6241f7045
Eric Herot <eric.github@herot.com>
2013-07-26 16:34:52 -0400

    Changed cookbook name in resource definition

--- resources/manage.rb
+++ resources/manage.rb
@@ -29,7 +29,7 @@ attribute :data_bag, :kind_of => String, :default => "users"
 attribute :search_group, :kind_of => String, :name_attribute => true
 attribute :group_name, :kind_of => String, :name_attribute => true
 attribute :group_id, :kind_of => Integer, :required => true
-attribute :cookbook, :kind_of => String, :default => "users"
+attribute :cookbook, :kind_of => String, :default => "et_users"
 
 def initialize(*args)
   super
