1638d390e61a2a85a011476b538e810b13c3f63e
Jeff Byrnes <jeff@evertrue.com>
2014-02-12 10:59:13 -0500

    Use generic email address

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name             'et_users'
 maintainer       'EverTrue, Inc.'
-maintainer_email 'eric.herot@evertrue.com'
+maintainer_email 'devops@evertrue.com'
 license          'All rights reserved'
 description      'Installs/Configures et_users'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
